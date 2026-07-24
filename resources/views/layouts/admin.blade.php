9de1d79bc2028b9690b2d1c5ebe352685429a111
gian <gianallanflores14@gmail.com>
2026-07-24 19:50:57 +0800

    size navbar

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -27,10 +27,10 @@
 
         </div>
 
-        <nav class="mt-8">
+        <nav class="mt-8 space-y-3"> <!--gap sa navigation items-->
 
             <a href="{{ route('admin.dashboard') }}"
-                class="block px-6 py-3 transition {{ request()->routeIs('admin.dashboard') ? 'bg-blue-600 text-white' : 'hover:bg-blue-600 text-slate-100' }} rounded-lg">
+                class="block px-6 py-3 transition {{ request()->routeIs('admin.dashboard') ? 'bg-blue-600 text-white' : 'hover:bg-blue-600 text-slate-100' }} rounded-small">
                 <span class="flex items-center gap-3">
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="h-5 w-5">
                         <path d="M10 2.5L2 8v8a1 1 0 001 1h4a1 1 0 001-1V12h2v4a1 1 0 001 1h4a1 1 0 001-1V8l-8-5.5z" />
@@ -40,7 +40,7 @@
             </a>
 
             <a href="{{ route('admin.employees') }}"
-                class="block px-6 py-3 transition {{ request()->routeIs('admin.employees') ? 'bg-blue-600 text-white' : 'hover:bg-blue-600 text-slate-100' }} rounded-lg">
+                class="block px-6 py-3 transition {{ request()->routeIs('admin.employees') ? 'bg-blue-600 text-white' : 'hover:bg-blue-600 text-slate-100' }} rounded-small">
                 <span class="flex items-center gap-3">
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="h-5 w-5">
                         <path fill-rule="evenodd" d="M10 4.5a3 3 0 100 6 3 3 0 000-6zM3.5 13a6.5 6.5 0 0113 0v1.25a1.25 1.25 0 01-1.25 1.25H4.75A1.25 1.25 0 013.5 14.25V13z" clip-rule="evenodd" />
@@ -50,7 +50,7 @@
             </a>
 
             <a href="{{ route('admin.payroll') }}"
-                class="block px-6 py-3 transition {{ request()->routeIs('admin.payroll') ? 'bg-blue-600 text-white' : 'hover:bg-blue-600 text-slate-100' }} rounded-lg">
+                class="block px-6 py-3 transition {{ request()->routeIs('admin.payroll') ? 'bg-blue-600 text-white' : 'hover:bg-blue-600 text-slate-100' }} rounded-small">
                 <span class="flex items-center gap-3">
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="h-5 w-5">
                         <path d="M10 2a7 7 0 100 14 7 7 0 000-14zm1 10.93V14a1 1 0 11-2 0v-1.07a4.01 4.01 0 01-2.47-1.81 1 1 0 111.74-.98A2.01 2.01 0 0010 11a2 2 0 002-2 1 1 0 112 0 4 4 0 01-4 4 4 4 0 01-1.27-.22A2.99 2.99 0 0111 12.93z" />
@@ -62,7 +62,7 @@
             @if(Auth::guard('admin')->user()->role != 'Assistant HR')
 
             <a href="{{ route('admin.payroll-history') }}"
-                class="block px-6 py-3 transition {{ request()->routeIs('admin.payroll-history') ? 'bg-blue-600 text-white' : 'hover:bg-blue-600 text-slate-100' }} rounded-lg">
+                class="block px-6 py-3 transition {{ request()->routeIs('admin.payroll-history') ? 'bg-blue-600 text-white' : 'hover:bg-blue-600 text-slate-100' }} rounded-small">
                 <span class="flex items-center gap-3">
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="h-5 w-5">
                         <path fill-rule="evenodd" d="M6 2a1 1 0 00-1 1v2H4a2 2 0 00-2 2v8a2 2 0 002 2h12a2 2 0 002-2V7a2 2 0 00-2-2h-1V3a1 1 0 00-1-1H6zm8 3V3h-2v2H8V3H6v2H4v2h12V5h-2z" clip-rule="evenodd" />
@@ -72,7 +72,7 @@
             </a>
 
             <a href="{{ route('admin.activity-log') }}"
-                class="block px-6 py-3 transition {{ request()->routeIs('admin.activity-log') ? 'bg-blue-600 text-white' : 'hover:bg-blue-600 text-slate-100' }} rounded-lg">
+                class="block px-6 py-3 transition {{ request()->routeIs('admin.activity-log') ? 'bg-blue-600 text-white' : 'hover:bg-blue-600 text-slate-100' }} rounded-small">
                 <span class="flex items-center gap-3">
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="h-5 w-5">
                         <path d="M6 3a1 1 0 00-1 1v12a1 1 0 001 1h8a1 1 0 001-1V4a1 1 0 00-1-1H6zm2 3a1 1 0 011-1h2a1 1 0 110 2H9a1 1 0 01-1-1zm0 4a1 1 0 011-1h4a1 1 0 110 2H9a1 1 0 01-1-1zm0 4a1 1 0 011-1h4a1 1 0 110 2H9a1 1 0 01-1-1z" />
@@ -86,7 +86,7 @@
             @if(Auth::guard('admin')->user()->role == 'Admin')
 
             <a href="{{ route('admin.add-admin') }}"
-                class="block px-6 py-3 transition {{ request()->routeIs('admin.add-admin') ? 'bg-blue-600 text-white' : 'hover:bg-blue-600 text-slate-100' }} rounded-lg">
+                class="block px-6 py-3 transition {{ request()->routeIs('admin.add-admin') ? 'bg-blue-600 text-white' : 'hover:bg-blue-600 text-slate-100' }} rounded-small">
                 <span class="flex items-center gap-3">
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="h-5 w-5">
                         <path fill-rule="evenodd" d="M10 5a3 3 0 100 6 3 3 0 000-6zm4 7a4 4 0 10-8 0v1H5a1 1 0 100 2h10a1 1 0 100-2h-1v-1zM10 3a7 7 0 00-7 7v3a1 1 0 001 1h1a1 1 0 100-2H4v-1a5 5 0 0110 0v1h-1a1 1 0 100 2h1a1 1 0 001-1v-3a7 7 0 00-7-7z" clip-rule="evenodd" />
